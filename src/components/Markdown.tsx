6e32ded55dc185358471f360839fbda8f36382f1
anthonylorendeaux <antho@PC-Antho.localdomain>
2022-03-09 22:14:48 +0100

    Add remarkJS

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import {EditorContext, EditorContextType} from '../EditorContext';
+import ReactMarkdown from 'react-markdown'
 
 export const Markdown: React.FC = () => {
     const { text } = React.useContext(EditorContext) as EditorContextType;  
     return (
         <div className="w-4/5  h-full">
-            <p> {text}</p>
+            <ReactMarkdown>{text}</ReactMarkdown>
         </div>
     )
 }
