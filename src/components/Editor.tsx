2ab97ad1c5a922678b0271cfe1619c23aad6f16b
anthonylorendeaux <antho@PC-Antho.localdomain>
2022-03-19 12:09:16 +0100

    Add codemirror to Editor component

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import {EditorContext, EditorContextType} from '../EditorContext';
+import CodeMirror from "@uiw/react-codemirror";
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { languages } from '@codemirror/language-data';
 
 export const Editor: React.FC = () => {
     const { text, updateText } = React.useContext(EditorContext) as EditorContextType;
 
-    function handleTextChange(event: any) {
-        updateText(event.target.value);
+    function handleTextChange(value: string) {
+        updateText(value);
     }
     
     return (
         <div className="w-3/5  h-full">
-            <textarea className="h-4/5 w-full" value={text} onChange={handleTextChange}></textarea>
+            <CodeMirror
+                className="h-4/5 w-full"
+                value={text}
+                onChange={(value, viewUpdate) => {
+                    handleTextChange(value)
+                }}
+                extensions={[markdown({ base: markdownLanguage, codeLanguages: languages })]}
+            />
         </div>
     )
 }
